10d479e1a18f435a17d33bcee35121fc9421d9a2
pao <ameridad2@miumg.edu.gt>
2022-10-28 11:08:32 -0600

    cambios

--- src/app/Inventario/Inventario.component.ts
+++ src/app/Inventario/Inventario.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-Inventario',
   templateUrl: './Inventario.component.html',
   styleUrls: ['./Inventario.component.css']
 })
 export class InventarioComponent implements OnInit {
+  inventario : any []=[];
 
-  constructor() { }
-
+  constructor(private http: HttpClient) { }
+  
   
 
   ngOnInit() {
+    this.http.get("http://localhost:8080/inventario/list").subscribe((resp:any)=>{
+    if(resp==null){
+          console.log("error no existe usuario o contraseña")
+      }
+        else{
+          this.inventario = resp;
+        }
+    });
   }
 
 }
